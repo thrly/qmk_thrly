1e9aa0669557eda57803454f7cc6a97b32feace1
thrly <o.thrly@gmail.com>
2025-09-04 14:52:37 +0100

    Update keymap

--- keyboards/planck/rev6_drop/keymaps/thrly_plnck/keymap.c
+++ keyboards/planck/rev6_drop/keymaps/thrly_plnck/keymap.c
@@ -10,28 +10,32 @@ enum planck_layers {
   _ADJUST
 };
 
-#define NAVIGATION MO(_NAVIGATION)
-#define SYMBOL MO(_SYMBOL)
+/* enum {TD_CAPS}; */
+
+/* #define SYMBOL MO(_SYMBOL) */
 
 /* COMBOS */
 const uint16_t PROGMEM combo_esc[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_colon[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_dash[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM combo_tab[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM combo_underscore[] = {KC_C, KC_V, COMBO_END};
+
+const uint16_t PROGMEM combo_quote[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_dquote[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM combo_del[] = {KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM combo_underscore[] = {KC_N, KC_M, COMBO_END};
-const uint16_t PROGMEM combo_dash[] = {KC_R, KC_T, COMBO_END};
-const uint16_t PROGMEM combo_equal[] = {KC_V, KC_B, COMBO_END};
-/* const uint16_t PROGMEM combo_colon[] = {KC_L, KC_SCLN, COMBO_END}; */
-/* const uint16_t PROGMEM combo_quote[] = {KC_U, KC_J, COMBO_END}; */
+const uint16_t PROGMEM combo_equal[] = {KC_M, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_esc, KC_ESC),
-    COMBO(combo_tab, KC_TAB),
-    COMBO(combo_del, KC_DEL),
-    COMBO(combo_underscore, S(KC_MINUS)),
+    COMBO(combo_colon, KC_COLN),
     COMBO(combo_dash, KC_MINUS),
-    COMBO(combo_equal, KC_EQUAL),
-    /* COMBO(combo_colon, KC_COLN), */
-    /* COMBO(combo_quote, KC_QUOT) */
+    COMBO(combo_tab, KC_TAB),
+    COMBO(combo_underscore, S(KC_MINUS)),
+    COMBO(combo_quote, KC_QUOT),
+    COMBO(combo_dquote, KC_AT),
+    COMBO(combo_del, KC_DEL),
+    COMBO(combo_equal, KC_EQUAL)
 };
 
 /* HOME-ROW MODS */
@@ -45,6 +49,12 @@ combo_t key_combos[] = {
 #define HOME_K RSFT_T(KC_K)
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
+
+// Tap Dance definitions
+/* tap_dance_action_t tap_dance_actions[] = { */
+/*     // Tap once for Caps Word, twice for Caps Lock */
+/*     [TD_CAPS] = ACTION_TAP_DANCE_DOUBLE(CW_TOGG, KC_CAPS), */
+/* }; */
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -61,10 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
     [_BASE] = LAYOUT_planck_1x2uC(
-        KC_Q,    KC_W,    KC_E,    KC_R,        KC_T,      KC_TAB,       CW_TOGG,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        HOME_A,  HOME_S,  HOME_D,  HOME_F,      KC_G,      QK_GESC,      KC_DEL,       KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,        KC_B,      SC_LSPO,      SC_RSPC,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LCTL, KC_LGUI, KC_LALT, NAVIGATION,  KC_SPC,      LT(_NUMBER,KC_ENT),       KC_BSPC, SYMBOL,  KC_RALT, KC_RGUI, KC_RCTL
+        KC_Q,    KC_W,    KC_E,    KC_R,        KC_T,                  KC_TAB,       CW_TOGG,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        HOME_A,  HOME_S,  HOME_D,  HOME_F,      KC_G,                  QK_GESC,      KC_CAPS,  KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,        KC_B,                  SC_LSPO,      SC_RSPC,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT,  LT(_NAVIGATION,KC_SPC),      LT(_NUMBER,KC_ENT),     LT(_SYMBOL,KC_BSPC),KC_RSFT, KC_RALT, KC_RGUI, KC_RCTL
     ),
 
 /* NAVIGATION
@@ -115,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
     [_NUMBER] = LAYOUT_planck_1x2uC(
-        KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,      KC_F24,   KC_CALC,      KC_F6,    KC_7,    KC_F8,   KC_F9,  KC_F10,
-        KC_1,    KC_2,    KC_3,     KC_4,     KC_5,       KC_F22,   KC_F23,       KC_6,     KC_7,    KC_8,    KC_9,   KC_0,
-        KC_PEQL, KC_DOT,  KC_PCMM,  KC_PPLS,  KC_PMNS,    KC_F20,   KC_F21,       KC_PAST,  KC_PSLS, KC_PCMM, KC_DOT, KC_PEQL,
+        KC_F1,   KC_F2,   KC_F3,    KC_F4,    KC_F5,      LGUI(KC_TAB), KC_CALC,      KC_F6,    KC_7,    KC_F8,   KC_F9,  KC_F10,
+        KC_1,    KC_2,    KC_3,     KC_4,     KC_5,       KC_F23,   KC_F24,       KC_6,     KC_7,    KC_8,    KC_9,   KC_0,
+        KC_PEQL, KC_DOT,  KC_PCMM,  KC_PPLS,  KC_PMNS,    KC_F20,   KC_F22,       KC_PAST,  KC_PSLS, KC_PCMM, KC_DOT, KC_PEQL,
         KC_F11,  KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
@@ -145,7 +155,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _NAVIGATION, _SYMBOL, _ADJUST);
 }
 
-// Key overrides
+// KEY OVERRIDES
 //
 // Shift + Backspace = Delete
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
@@ -153,3 +163,5 @@ const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC
 const key_override_t *key_overrides[] = {
 	&delete_key_override
 };
+
+
